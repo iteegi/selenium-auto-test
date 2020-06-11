d8411d2ed7dc4a3e3c77cceb1205d4f2bd04de08
J D <jd@gmail.com>
2020-06-11 18:14:12 +0300

    added factory class WebDriverWait

--- base_html/webdriver/support/wait.py
+++ base_html/webdriver/support/wait.py
@@ -2,7 +2,18 @@
 
 from . __private.__wait import __web_driver_wait
 
+from patterns.generating.factory_method import FactoryMethod
+from selenium.webdriver.support.wait import WebDriverWait as WDW
 
+
+# FIXME убрать метод
 def web_driver_wait(*args, **kwargs):
     """Get web driver wait."""
     return __web_driver_wait(*args, **kwargs)
+
+
+@FactoryMethod
+class WebDriverWait():
+    """Factory Method for WebDriverWait from Selenium."""
+
+    WDWSelenium = WDW
