1e2ee3eba31755a66782e7d0d49b8fbb09d23850
J D <jd@gmail.com>
2020-06-15 00:54:36 +0300

    updated annotation and description of methods in class ExecScriptReturnZero

--- base_html/webdriver/support/ec/custom/exec_script.py
+++ base_html/webdriver/support/ec/custom/exec_script.py
@@ -1,18 +1,29 @@
 """Custom methods implementing expected conditions."""
 
-# TODO: избавиться от селениум
-from selenium.webdriver.remote.webdriver import WebDriver
+from typing import Type
+
+from base_html.webdriver.support.ec.interfaces.iec import IExpectedConditions
+from base_html.webdriver.remote.fabric import BaseWebDriverFabric
+
+base_webdriver = Type[BaseWebDriverFabric.REMOTE_WEB_DRIVER_SELENIUM]
 
 
-class ExecScriptReturnZero(object):
+class ExecScriptReturnZero(IExpectedConditions):
     """The supplied external script returns zero."""
 
     def __init__(self, script: str) -> None:
-        """Initialize."""
+        """Initialize.
+
+        script - code to be executed on the page. Code should return a number.
+        """
         self._script = script
 
-    def __call__(self, driver:  WebDriver) -> bool:
-        """Call when the instance is “called” as a function."""
+    def __call__(self,
+                 driver: base_webdriver) -> bool:
+        """Call when an instance is "called" as a function.
+
+        In the Until method of the WebDriverWait class.
+        """
         element: int = driver.execute_script(self._script)
         if element == 0:
             return True
