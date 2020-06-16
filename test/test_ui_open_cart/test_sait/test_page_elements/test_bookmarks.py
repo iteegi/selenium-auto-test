6bbedda1a5e5d0ee294282dd76704e0d93ebdb6a
J D <jd@gmail.com>
2020-06-17 02:57:33 +0300

    added parameterization for test_wishlist

--- test/test_ui_open_cart/test_sait/test_page_elements/test_bookmarks.py
+++ test/test_ui_open_cart/test_sait/test_page_elements/test_bookmarks.py
@@ -1,10 +1,13 @@
 """Test bookmarks."""
 
+import pytest
 from page_object.page_elements.bookmarks import BookmarksElmnt
 
 
-def test_wishlist(wd):
+# TODO: исправить scope для фикстуры. для каждого параметра новый браузер.
+@pytest.mark.parametrize('f', [1, 2, 0])
+def test_wishlist(wd, f):
     """Test for correct text change in the wishlist."""
     search_elmt = BookmarksElmnt(wd)
     search_elmt.get_page("http://demo-opencart.ru/index.php")
-    assert search_elmt.check_wishlist(quantity=4)
+    assert search_elmt.check_wishlist(quantity=f)
