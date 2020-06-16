258313b9d2d0685c49edcca1d8019a1cf250e466
J D <jd@gmail.com>
2020-06-17 01:39:27 +0300

    in the class BookmarksElmnt removed all the excess

--- test/test_ui_open_cart/page_object/page_elements/bookmarks.py
+++ test/test_ui_open_cart/page_object/page_elements/bookmarks.py
@@ -2,7 +2,8 @@
 
 from base_html.html.base import HTMLPage
 from base_html.webdriver.common.by import ByFabric
-from base_html.html.helpers.checkers.clickers import ClickAndCheck
+from base_html.html.helpers.checkers.clickers.click_and_check import (
+    ClickAndCheck)
 from base_html.webdriver.support.ec.fabric import ECFabric
 from base_html.html.helpers.iterators.get_element import OneElementOutOfMany
 from base_html.html.helpers.comparators.check_text.check_text import (
@@ -10,44 +11,26 @@ from base_html.html.helpers.comparators.check_text.check_text import (
 
 
 class BookmarksElmnt(HTMLPage):
-    """SearchElmnt class."""
+    """BookmarksElmnt class."""
 
-    CONTROL_BUTTON = (
-        ByFabric.by(ByFabric.OS.BY).XPATH,
-        "//*[@id='content']/div[2]/div[1]/div/div[3]/button[2]")
     INVESTIGATING_TEXT_ELEMENT = (ByFabric.by(ByFabric.OS.BY).XPATH,
                                   "//a[@id='wishlist-total']")
 
-    CONTROL_BUTTON_2 = (
+    CONTROL_BUTTON = (
         ByFabric.by(ByFabric.OS.BY).XPATH,
         "//*[@data-original-title='В закладки']")
 
     def check_wishlist(self, quantity=1, time=10):
         text = "Закладки ({0})".format(quantity)
-        # h = ECFabric.ec(ECFabric.Choice.EX, "jyfggy")
-        # print("----{}".format(h))
         check = ClickAndCheck(self.driver,
                               ECFabric.EXEC_SCRIPT_RETURN_ZERO(
                                   "return window.pageYOffset"),
                               OneElementOutOfMany(
                                   self.driver,
-                                  self.CONTROL_BUTTON_2,
+                                  self.CONTROL_BUTTON,
                                   quantity,
                                   time),
                               Checker(self.driver,
                                       self.INVESTIGATING_TEXT_ELEMENT,
                                       text, time))
         return check.run()
-
-    def check_wishlist2(self, quantity=1, time=10):
-        """Check if the text in the wish list has changed correctly."""
-        text = "Закладки ({0})".format(quantity)
-
-        # return self.click_and_check_text(self.CONTROL_BUTTON_2,
-        #                                  self.INVESTIGATING_TEXT_ELEMENT,
-        #                                  text,
-        #                                  quantity)
-
-        self.click_and_check_text(self.CONTROL_BUTTON_2, quantity, time)
-        return self.check_text_matches(self.INVESTIGATING_TEXT_ELEMENT,
-                                       text, time)
