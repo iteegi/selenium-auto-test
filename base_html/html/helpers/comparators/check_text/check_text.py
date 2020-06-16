513d2d464a02f5ef563e8d71deefd69f29358eff
J D <jd@gmail.com>
2020-06-17 01:21:15 +0300

    added to class TextToBePresentInElement interface

--- base_html/html/helpers/comparators/check_text/check_text.py
+++ base_html/html/helpers/comparators/check_text/check_text.py
@@ -2,6 +2,8 @@
 
 from typing import Type, Tuple
 
+from .interfaces.icheckertext import ICheckText
+
 from base_html.webdriver.remote.webdriver.fabric import BaseWebDriverFabric
 from base_html.webdriver.support.wait import WebDriverWait
 from base_html.webdriver.support.ec.fabric import ECFabric
@@ -9,7 +11,7 @@ from base_html.webdriver.support.ec.fabric import ECFabric
 base_webdriver = Type[BaseWebDriverFabric.REMOTE_WEB_DRIVER_SELENIUM]
 
 
-class TextToBePresentInElement():
+class TextToBePresentInElement(ICheckText):
     """TextToBePresentInElement class."""
 
     def __init__(self,
@@ -23,7 +25,7 @@ class TextToBePresentInElement():
         self.__text = text
         self.__time = time
 
-    def check_text(self):
+    def check_text(self) -> bool:
         """Check if the given text matches the text on the given webelement."""
         return WebDriverWait.WDW_SELENIUM(self.__driver, self.__time).until(
             ECFabric.EC_SELENIUM.text_to_be_present_in_element(self.__locator,
