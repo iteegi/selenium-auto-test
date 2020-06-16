7223dbc6c1e79acaca5aff56d03c3e58a4a7f87b
J D <jd@gmail.com>
2020-06-16 17:44:37 +0300

    added annotations to the TextToBePresentInElement class

--- base_html/html/helpers/comparators/check_text/check_text.py
+++ base_html/html/helpers/comparators/check_text/check_text.py
@@ -1,21 +1,31 @@
 """TextToBePresentInElement."""
 
-from base_html.webdriver.support.wait import web_driver_wait
-from base_html.webdriver.support.expected_conditions import get_EC as EC
+from typing import Type, Tuple
+
+from base_html.webdriver.remote.webdriver.fabric import BaseWebDriverFabric
+from base_html.webdriver.support.wait import WebDriverWait
+from base_html.webdriver.support.ec.fabric import ECFabric
+
+base_webdriver = Type[BaseWebDriverFabric.REMOTE_WEB_DRIVER_SELENIUM]
 
 
 class TextToBePresentInElement():
     """TextToBePresentInElement class."""
 
-    def __init__(self, driver, text_element, text, time=10):
+    def __init__(self,
+                 driver: base_webdriver,
+                 locator: Tuple[str, str],
+                 text: str,
+                 time: float = 10):
+        """Initialize."""
         self.__driver = driver
-        self.__text_element = text_element
+        self.__locator = locator
         self.__text = text
         self.__time = time
 
     def check_text(self):
-        """Check if the text matches."""
-        return web_driver_wait(self.__driver, self.__time).until(
-            EC().text_to_be_present_in_element(self.__text_element,
-                                               self.__text),
-            message=f"Can't find elements by locator {self.__text_element}")
+        """Check if the given text matches the text on the given webelement."""
+        return WebDriverWait.WDW_SELENIUM(self.__driver, self.__time).until(
+            ECFabric.EC_SELENIUM.text_to_be_present_in_element(self.__locator,
+                                                               self.__text),
+            message=f"Can't find elements by locator {self.__locator}")
