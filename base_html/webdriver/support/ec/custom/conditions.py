db9ab16737f7d9e4a64bf92fd26f9a996f579531
J D <jd@gmail.com>
2020-07-02 00:12:02 +0300

    added annotations to ElementLocatedCascade

--- base_html/webdriver/support/ec/custom/conditions.py
+++ base_html/webdriver/support/ec/custom/conditions.py
@@ -1,26 +1,31 @@
 """Custom expected conditions."""
 
-from typing import Type
+from typing import Type, Tuple
 
 from base_html.webdriver.support.ec.interfaces.iec import IExpectedConditions
 from base_html.webdriver.remote.webdriver.fabric import BaseWebDriverFabric
+from base_html.webdriver.remote.webelement.fabric import BaseWebElementFabric
 
 base_webdriver = Type[BaseWebDriverFabric.REMOTE_WEB_DRIVER_SELENIUM]
+base_webelement = Type[BaseWebElementFabric.REMOTE_WEB_ELEMENT_SELENIUM]
 
 
 class ElementLocatedCascade(IExpectedConditions):
-    """Find an element inside another element.
+    """Find an element inside another element."""
 
-    target - selector where you want to find another element
-    locator - element selector
-    """
+    def __init__(self,
+                 target: Tuple[str, str],
+                 locator: Tuple[str, str]) -> None:
+        """Initialize.
 
-    def __init__(self, target, locator):
-        """Initialize."""
+        target - selector where you want to find another element
+        locator - element selector
+        """
         self.__target = target
         self.__locator = locator
 
-    def __call__(self, driver):
+    def __call__(self,
+                 driver: base_webdriver) -> base_webelement:
         """Call when an instance is "called" as a function.
 
         In the Until method of the WebDriverWait class.
