757d158a51de52fdc045c10df0c2706f8a44dc9b
J D <jd@gmail.com>
2020-07-02 00:25:05 +0300

    added ElementsLocatedCascade

--- base_html/webdriver/support/ec/custom/conditions.py
+++ base_html/webdriver/support/ec/custom/conditions.py
@@ -1,6 +1,6 @@
 """Custom expected conditions."""
 
-from typing import Type, Tuple
+from typing import Type, Tuple, List
 
 from base_html.webdriver.support.ec.interfaces.iec import IExpectedConditions
 from base_html.webdriver.remote.webdriver.fabric import BaseWebDriverFabric
@@ -32,4 +32,29 @@ class ElementLocatedCascade(IExpectedConditions):
         """
         return driver.find_element(
             *self.__target).find_element(
+                *self.__locator)
+
+
+class ElementsLocatedCascade(IExpectedConditions):
+    """Find an elements inside another element."""
+
+    def __init__(self,
+                 target: Tuple[str, str],
+                 locator: Tuple[str, str]) -> None:
+        """Initialize.
+
+        target - selector where you want to find another elements
+        locator - elements selector
+        """
+        self.__target = target
+        self.__locator = locator
+
+    def __call__(self,
+                 driver: base_webdriver) -> List[base_webelement]:
+        """Call when an instance is "called" as a function.
+
+        In the Until method of the WebDriverWait class.
+        """
+        return driver.find_element(
+            *self.__target).find_elements(
                 *self.__locator)
