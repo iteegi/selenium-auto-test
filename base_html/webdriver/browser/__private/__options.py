8c7f89a2dac0173e0a90c7703f1393a429a13dfe
J D <jd@gmail.com>
2020-07-06 00:24:36 +0300

    deleted folder __private

--- base_html/webdriver/browser/__private/__options.py
+++
@@ -1,7 +0,0 @@
-"""Selenium options."""
-
-from selenium.webdriver.chrome.options import Options
-
-
-def __get_options(*args, **kwargs):
-    return Options(*args, **kwargs)
--- base_html/webdriver/browser/__private/__webdriver.py
+++
@@ -1,7 +0,0 @@
-"""Selenium webdriver."""
-
-from selenium import webdriver
-
-
-def __get_chrome_wd(*args, **kwargs):
-    return webdriver.Chrome(*args, **kwargs)
