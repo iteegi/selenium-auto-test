ba544cc68a6db3a75e759f2385e2591fefc44e44
J D <jd@gmail.com>
2020-07-06 00:19:55 +0300

    fixed annotation

--- test/test_ui_open_cart/conftest.py
+++ test/test_ui_open_cart/conftest.py
@@ -2,12 +2,16 @@
 
 import pytest
 import time
+from typing import Iterator, Type
 
 from base_html.webdriver import WebDriver, Options
+from base_html.webdriver.remote.webdriver.fabric import BaseWebDriverFabric
+
+base_webdriver = Type[BaseWebDriverFabric.REMOTE_WEB_DRIVER_SELENIUM]
 
 
 @pytest.fixture(scope="session")
-def wd() -> None:
+def wd() -> Iterator[base_webdriver]:
     """Web driver creation fixture."""
     o = Options.Options_SELENIUM()
     o.add_argument("start-maximized")
