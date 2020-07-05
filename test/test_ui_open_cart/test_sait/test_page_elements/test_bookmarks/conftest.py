9b0100c67b762419a3c94074f5db68b3e3596c8d
J D <jd@gmail.com>
2020-07-05 23:38:52 +0300

    added annotation

--- test/test_ui_open_cart/test_sait/test_page_elements/test_bookmarks/conftest.py
+++ test/test_ui_open_cart/test_sait/test_page_elements/test_bookmarks/conftest.py
@@ -2,12 +2,18 @@
 
 import pytest
 import time
+from typing import Iterator, Type, TypeVar
 
 from base_html.webdriver import WebDriver, Options
+from base_html.webdriver.remote.webdriver.fabric import BaseWebDriverFabric
+
+base_webdriver = Type[BaseWebDriverFabric.REMOTE_WEB_DRIVER_SELENIUM]
+
+T = TypeVar('T')
 
 
 @pytest.fixture(scope="session")
-def start():
+def start() -> Iterator[base_webdriver]:
     """Web driver creation fixture."""
     o = Options.Options_SELENIUM()
     o.add_argument("start-maximized")
@@ -18,7 +24,7 @@ def start():
 
 
 @pytest.fixture(scope="function")
-def wd(start):
+def wd(start) -> Iterator[T]:
     """Clear your cookies before each test."""
     yield start
     start.delete_all_cookies()
