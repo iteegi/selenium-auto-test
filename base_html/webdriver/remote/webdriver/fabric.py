80476d94856aa504b79fcfce7bb6ef2290fa8c65
J D <jd@gmail.com>
2020-07-06 00:50:15 +0300

    deleted TODO

--- base_html/webdriver/remote/webdriver/fabric.py
+++ base_html/webdriver/remote/webdriver/fabric.py
@@ -4,8 +4,7 @@
 from selenium.webdriver.remote.webdriver import WebDriver
 from patterns.generating.factory_method import FactoryMethod
 
-# TODO: возможно это не самый лучший способ для аннотации базового класса
-# для webdriver'a
+
 @FactoryMethod
 class BaseWebDriverFabric():
     """Factory Method for base class Web Driver."""
