dcb05e9473d0b0fabbfed564340d9f7fcbb0ba7b
J D <jd@gmail.com>
2020-07-07 01:43:45 +0300

    added call execute_script_and_return_item and assert

--- test/test_ui_jqueryui/test_site/test_page_elements/test_selectmenu/test_selectmenu_default.py
+++ test/test_ui_jqueryui/test_site/test_page_elements/test_selectmenu/test_selectmenu_default.py
@@ -1,6 +1,5 @@
 """Test select menu default."""
 
-# from test import test_ui_jqueryui
 from page_object.page_elements.select_menu import SelectMenuDefault
 
 
@@ -8,3 +7,6 @@ def test_select_menu_default(wd):
     """Check if an additional menu is displayed."""
     page = SelectMenuDefault(wd)
     page.get_page("https://jqueryui.com/selectmenu/")
+    el = page.execute_script_and_return_item(
+        '$( "#speed" ).selectmenu( "open" );')
+    assert el.is_displayed()
