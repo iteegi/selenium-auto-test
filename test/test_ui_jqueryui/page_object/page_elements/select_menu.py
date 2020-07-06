8993f967c86387cf08d5f38d6d51680e4df543f1
J D <jd@gmail.com>
2020-07-06 22:12:08 +0300

    created function switch_to_frame

--- test/test_ui_jqueryui/page_object/page_elements/select_menu.py
+++ test/test_ui_jqueryui/page_object/page_elements/select_menu.py
@@ -1,1 +1,15 @@
 """Page object for select menu."""
+
+from base_html.html.base import HTMLPage
+from base_html.webdriver.common.by import By
+
+
+class SelectMenuDefault(HTMLPage):
+    """Class describing selectmenu for standard functionality."""
+
+    IFRAME = (By.BY_SELENIUM.TAG_NAME, "iframe")
+    SELECTMENU = (By.BY_SELENIUM.CLASS_NAME, "ui-selectmenu-open")
+
+    def switch_to_frame(self, time=10):
+        frm = self.find_elmnt(self.IFRAME, time)
+        self.driver.switch_to.frame(frm)
