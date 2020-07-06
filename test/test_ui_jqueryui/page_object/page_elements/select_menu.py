c360320a20ba1f5baae25b364abc06549e2b1994
J D <jd@gmail.com>
2020-07-07 01:39:02 +0300

    Execute the script and find the item that appears.

--- test/test_ui_jqueryui/page_object/page_elements/select_menu.py
+++ test/test_ui_jqueryui/page_object/page_elements/select_menu.py
@@ -1,7 +1,12 @@
 """Page object for select menu."""
 
+from typing import Type
+
 from base_html.html.base import HTMLPage
 from base_html.webdriver.common.by import By
+from base_html.webdriver.remote.webelement.fabric import BaseWebElementFabric
+
+base_webelement = Type[BaseWebElementFabric.REMOTE_WEB_ELEMENT_SELENIUM]
 
 
 class SelectMenuDefault(HTMLPage):
@@ -10,6 +15,11 @@ class SelectMenuDefault(HTMLPage):
     IFRAME = (By.BY_SELENIUM.TAG_NAME, "iframe")
     SELECTMENU = (By.BY_SELENIUM.CLASS_NAME, "ui-selectmenu-open")
 
-    def switch_to_frame(self, time=10):
+    def switch_to_frame_q(self,
+                          script: str,
+                          time: float = 10) -> base_webelement:
+        """Execute the script and find the item that appears."""
         frm = self.find_elmnt(self.IFRAME, time)
-        self.driver.switch_to.frame(frm)
+        self.switch_to_frame(frm)
+        self.exec_script(script)
+        return self.find_elmnt(self.SELECTMENU, time)
