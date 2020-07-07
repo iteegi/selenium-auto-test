3dc6fda0f0dae7570dbd5424e0c5ac116c7b233b
J D <jd@gmail.com>
2020-07-07 17:10:09 +0300

    created find_all_cells

--- test/test_ui_jqueryui/page_object/page_elements/sortable_portlets.py
+++ test/test_ui_jqueryui/page_object/page_elements/sortable_portlets.py
@@ -1,1 +1,20 @@
 """Page object for sortable portlets."""
+
+from base_html.html.base import HTMLPage
+from base_html.webdriver.common.by import By
+
+
+class SortablePortlets(HTMLPage):
+
+    IFRAME = (By.BY_SELENIUM.TAG_NAME, "iframe")
+    COLUMNS = (By.BY_SELENIUM.CSS_SELECTOR, "div.ui-sortable")
+    CELLS = (By.BY_SELENIUM.XPATH, ".//*")
+
+    def find_all_cells(self, time=10):
+        all_columns = self.find_elmnts(self.COLUMNS, time)
+        all_cells = []
+
+        for i in range(len(all_columns)):
+            all_cells.append(all_columns[i].find_elmnts(self.CELLS, time))
+
+        return all_cells
