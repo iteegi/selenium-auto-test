12eae91636796ea2d810ae0d513ca21c60520119
J D <jd@gmail.com>
2020-07-09 14:31:49 +0300

    deleted unused code

--- test/test_ui_jqueryui/page_object/page_elements/sortable_portlets.py
+++ test/test_ui_jqueryui/page_object/page_elements/sortable_portlets.py
@@ -2,61 +2,33 @@
 
 from base_html.html.base import HTMLPage
 from base_html.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-import time as t
+from base_html.webdriver.common.action_chains import ActionChains
+from base_html.webdriver.support.wait.fabric import WebDriverWait
+from base_html.webdriver.support.ec.fabric import ECFabric
 
 
 class SortablePortlets(HTMLPage):
 
     IFRAME = (By.BY_SELENIUM.TAG_NAME, "iframe")
     COLUMNS = (By.BY_SELENIUM.CSS_SELECTOR, "div.ui-sortable")
-    CELLS = (By.BY_SELENIUM.XPATH, ".//*")
+    CELLS = (By.BY_SELENIUM.CSS_SELECTOR, "div.portlet-header")
 
     def find_all_cells(self, time=10):
+        """Return a list of items before and after the action."""
         frm = self.find_elmnt(self.IFRAME, time)
-        print(f"_____ {frm.location['y']}")
-        a = self.driver.execute_script("arguments[0].scrollIntoView(true);return window.pageYOffset",
-                                   frm)
 
-        print(f"-------{a}--------")
+        WebDriverWait.WDW_SELENIUM(
+            self.driver, time).until(
+                ECFabric.SCROLL_TO_ELEMENT(frm))
+
         self.switch_to_frame(frm)
-        all_columns = self.find_elmnts(self.COLUMNS, time)
-        print(f"---->{len(all_columns)}")
-        all_cells = []
 
-        for i in range(len(all_columns)):
-            # print(f">>>---{i} ==== {len(all_columns[i])}")
+        first_cells = self.find_elmnts_cascade(self.COLUMNS, self.CELLS)
 
-            # TODO: может через cascade
-            j = all_columns[i].find_elements(By.BY_SELENIUM.CSS_SELECTOR, "div.portlet-header")
+        source_elmt = first_cells[0]
+        destination_elmt = first_cells[1]
 
-            # print(f"---{i} ==== {len(j)}")
-            for k in range(len(j)):
-                # print(f"====> {type(j[k])}")
-                # fo = j[k].get_attribute('class')
-                # print(f"====> {fo}")
-                all_cells.append(j[k])
-
-        print("================")
-        for i in range(len(all_cells)):
-            print(all_cells[i])
-        print("================")
-
-        lx = self.find_elmnts_cascade(self.COLUMNS, (By.BY_SELENIUM.CSS_SELECTOR, "div.portlet-header"))
-        print("================")
-        for i in range(len(lx)):
-            print(lx[i])
-        print("================")
-
-        source_elmt = all_cells[0]
-        destination_elmt = all_cells[1]
-
-        print(f"==== {source_elmt.text} ====")
-        print(f"==== {destination_elmt.text} ====")
-
-        # t.sleep(3)
-
-        act = ActionChains(self.driver)
+        act = ActionChains.ACTIONCHAINS_SELENIUM(self.driver)
         act.move_to_element(source_elmt)
         act.click_and_hold()
 
@@ -65,20 +37,10 @@ class SortablePortlets(HTMLPage):
                            - source_elmt.location['y'])
 
         act.perform()
-        # t.sleep(5)
 
         act.release()
         act.reset_actions()
-        # act.perform()
 
+        second_cells = self.find_elmnts_cascade(self.COLUMNS, self.CELLS)
 
-        lx2 = self.find_elmnts_cascade(self.COLUMNS, (By.BY_SELENIUM.CSS_SELECTOR, "div.portlet-header"))
-        print("================")
-        for i in range(len(lx2)):
-            print(lx2[i])
-        print("================")
-
-        # assert lx2 == lx
-        # return [lx, lx2]
-        # return 0
-        return all_cells
+        return [first_cells, second_cells]
