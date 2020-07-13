6b192e03e071e0a8492a1d77af79c7e7054a5241
J D <jd@gmail.com>
2020-07-14 02:19:05 +0300

    improved test for checking the movement of web elements

--- test/test_ui_jqueryui/test_site/test_page_elements/sortable_portlets/test_sortable_portlets.py
+++ test/test_ui_jqueryui/test_site/test_page_elements/sortable_portlets/test_sortable_portlets.py
@@ -1,11 +1,21 @@
 """Test sortable portlets."""
 
-from page_object.page_elements.sortable_portlets import SortablePortlets
+from page_object.page_elements.sortable_portlets import (
+    SortablePortlets, AllPlayLoads)
 
 
 def test_sortable_portlets(wd):
     """Check if an additional menu is displayed."""
     page = SortablePortlets(wd)
     page.get_page("https://jqueryui.com/sortable/#portlets")
+    page.switch_frame(page.IFRAME)
+    first_cells = page.find_elements(page.COLUMNS, page.CELLS)
+    source_elmt = first_cells[0]
+    destination_elmt = first_cells[1]
 
-    print(len(page.find_all_cells()))
+    AllPlayLoads.STANDART(page.driver, source_elmt, page.get_offset(
+        source_elmt, destination_elmt)).run()
+
+    second_cells = page.find_elements(page.COLUMNS, page.CELLS)
+
+    assert first_cells != second_cells
